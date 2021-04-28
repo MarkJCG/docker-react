f28be3cffbdc7c2e89922eb75fd10b6c0522d89d
Mark Christy George <mark.christy.george@rwth-aachen.de>
2021-04-28 09:17:40 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          Changed commits from feature branch!
         </a>
       </header>
     </div>
